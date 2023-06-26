9abc7e83857c8e0cf769e15a3d4280ca5e4e7f9c
john clampet <jsclampet@gmail.com>
2023-06-26 13:27:57 -0500

    deep dive into Zod library, integration with reactHookForms, and form validation ... last commit before "10-Project- Expense Tracker"

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,15 +1,32 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters!!!" }),
+  age: z
+    .number({ invalid_type_error: "Age field is required." })
+    .min(18, { message: "Age must be at least 18." }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+  console.log("ERRORS >>> ", errors);
 
-  const onSubmit = (data: FieldValues) => console.log(data);
+  const onSubmit = (data: FieldValues) => console.log("DATA >=== ", data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* All below was made possible with the following shortcut:
-          "div.mb-3>label.form-label+input.form-control"  
+      {/* 
+        All below was made possible with the following shortcut:
+        "div.mb-3>label.form-label+input.form-control"  
       */}
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
@@ -21,20 +38,24 @@ const Form = () => {
           type="text"
           className="form-control"
         />
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
       {/* shortcut: button.btn.btn-primary */}
-      <button className="btn btn-primary">Submit</button>
+      <button disabled={!isValid} type="submit" className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
 };
